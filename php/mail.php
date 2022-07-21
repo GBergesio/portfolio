c2c791ba218ff169a2488aad40981e8c71fe1223
Guillermo Bergesio <bergesiog1@gmail.com>
2022-07-21 14:18:37 -0300

    arreglo title

--- php/mail.php
+++ php/mail.php
@@ -4,7 +4,13 @@ $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
 
-$mailheader = "From:" .$name."<".$email.">"
+$mailheader = "From:" .$name."<".$email.">\r\n";
 
+$recipient = "bergesiog1@gmail.com";
+
+mail($recipient,$message,$mailheader)
+or die("Error!");
+
+echo"message send";
 
 ?>
